2bb4caa9a2d5f5bf5a55a314c52ce5d53fb6f404
philau2512 <dplau25122002@gmail.com>
2025-06-18 00:36:26 +0700

    week 3 solidity cơ bản

---
+++ week_3/bai_tap/bai_3.2/VotingEligibility.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+contract VotingEligibility {
+    uint public minAge = 18;
+    address public owner = msg.sender;
+
+    function checkEligibility(uint age) public view returns (bool) {
+        if (age >= minAge) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function updateMinAge(uint newMinAge) public {
+        require(msg.sender == owner, "Only owner can update min age");
+        minAge = newMinAge;
+    }
+}
